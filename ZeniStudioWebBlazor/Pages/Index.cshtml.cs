d49e791ff77ef717e387a634536a351c91d974ea
Nikita MKartinez <nikita.martinez@udla.edu.ec>
2022-11-13 21:47:25 -0500

    Edite

--- ZeniStudioWebBlazor/Pages/Index.cshtml.cs
+++ ZeniStudioWebBlazor/Pages/Index.cshtml.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using ZeniStudioWebBlazor.Data;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.EntityFrameworkCore;
+//using ZeniStudioWebBlazor.Data;
 
-namespace ZeniStudioWebBlazor.Pages
-{
-    public class IndexModel : PageModel
-    {
-        private readonly ZeniStudioWebBlazor.Data.ApplicationDbContext _context;
+//namespace ZeniStudioWebBlazor.Pages
+//{
+//    public class IndexModel : PageModel
+//    {
+//        private readonly ZeniStudioWebBlazor.Data.ApplicationDbContext _context;
 
-        public IndexModel(ZeniStudioWebBlazor.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+//        public IndexModel(ZeniStudioWebBlazor.Data.ApplicationDbContext context)
+//        {
+//            _context = context;
+//        }
 
-        public IList<Proyect> Proyect { get;set; } = default!;
+//        public IList<Proyect> Proyect { get;set; } = default!;
 
-        public async Task OnGetAsync()
-        {
-            if (_context.Proyect != null)
-            {
-                Proyect = await _context.Proyect.ToListAsync();
-            }
-        }
-    }
-}
+//        public async Task OnGetAsync()
+//        {
+//            if (_context.Proyect != null)
+//            {
+//                Proyect = await _context.Proyect.ToListAsync();
+//            }
+//        }
+//    }
+//}
